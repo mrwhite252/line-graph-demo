3ea4dc3976d54354ae90baca9d1c1e11f6399036
David Bai <baiminghan@gmail.com>
2022-09-13 16:08:02 +1200

    setup the realtime listener for firestore

---
+++ graph.js
@@ -0,0 +1,46 @@
+const update = (data) => {
+
+    console.log(data);
+}
+
+
+
+
+
+
+
+
+// data and firestore
+
+let data = [];
+
+// setup realtime listener
+
+db.collection('activities').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+
+        const doc = {
+            ...change.doc.data(),
+            id: change.doc.id
+        };
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case "modified":
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            default:
+                break;
+        }
+
+    });
+
+    update(data);
+})
