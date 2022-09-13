d50d324202c2cf16a6549f5599e4c3a2be6c0a30
David Bai <baiminghan@gmail.com>
2022-09-13 15:56:21 +1200

    connect to firebase and add the data

--- index.js
+++ index.js
@@ -27,4 +27,25 @@ btns.forEach(btn => {
 
         formAct.textContent = activity;
     })
+});
+
+// form submit
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const distance = parseInt(input.value);
+
+    if (distance) {
+        db.collection('activities').add({
+            distance: distance,
+            activity: activity,
+            date: new Date().toString()
+        }).then(() => {
+            error.textContent = '';
+            input.value = '';
+        })
+    } else {
+        error.textContent = 'Please enter a valid distance'
+    }
 })
